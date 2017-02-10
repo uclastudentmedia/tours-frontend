5b2ad949bdc230c74724382bf06cc382d4d54fad
Patrick Tran <patricktran@Patricks-MacBook-Pro.local>
2017-02-10 14:33:27 -0800

    fixed files

--- android/app/src/main/java/com/toursfrontend/MainApplication.java
+++ android/app/src/main/java/com/toursfrontend/MainApplication.java
@@ -27,11 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-<<<<<<< HEAD
           new MapsPackage()
-=======
-            new MapsPackage()
->>>>>>> master
       );
     }
   };
