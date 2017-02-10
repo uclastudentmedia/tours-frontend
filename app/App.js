6074c21a7b1777dd249dedab84b2aacd681f01ae
Patrick Tran <patricktran@Patricks-MacBook-Pro.local>
2017-02-10 14:54:59 -0800

    fixed app.js

--- app/App.js
+++ app/App.js
@@ -11,30 +11,6 @@ import MainMapView from'./Views/MainMapView'
 const styles = require( "../assets/css/style");
 
 export default class App extends Component {
-<<<<<<< HEAD
-  render() {
-    const { region } = this.props;
-    console.log(region);
-
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.map}
-          initialRegion={{
-            latitude: 34.0689,
-            longitude: -118.4452,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-        }}>
-        </MapView>
-          <Text style={styles.description}>
-              Description of location is here. Description of location is here. Description of location is here.
-              Description of location is here. Description of location is here. Description of location is here.
-              Description of location is here.
-          </Text>
-      </View>
-    );
-  }
-=======
     static get defaultProps(){
         return {
             title: 'Test'
@@ -51,7 +27,6 @@ export default class App extends Component {
             />
         );
     }
->>>>>>> master
 }
 
 //<View style={styles.container}>
