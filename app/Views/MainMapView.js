cc80fc4c02d664c92e364cb4b85337a779506a27
Erik Melton <meltonaerik@gmail.com>
2017-02-15 16:11:04 -0800

    Attempt at fixing d.b

--- app/Views/MainMapView.js
+++ app/Views/MainMapView.js
@@ -12,7 +12,6 @@ import {
     Image
 } from 'react-native';
 import MapView from 'react-native-maps';
-import {DistancePrioritize} from '../Utils'
 
 import LoadingView from './LoadingView';
 
@@ -21,7 +20,6 @@ const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 var {height, width} = Dimensions.get('window');
 var dataPop = [];
-var test = ['a','b'];
 
 export default class MainMapView extends Component {
 
@@ -29,19 +27,15 @@ export default class MainMapView extends Component {
 
     async componentWillMount(){
         this.getPosition();
-    }
 
-    async componentDidMount(){
         try {
             let value = await AsyncStorage.getItem('data');
             let val = JSON.parse(value);
-            let val2 = DistancePrioritize(this.state.initialPosition.coords.latitude, this.state.initialPosition.coords.longitude);
-            console.log("ADFADFAF");
-            console.log(val2);
             if(val !== null){
                 this.setState({
                     data: val
                 });
+
                 var temp = val.results;
                 for(var i = 0; i < 10; i++)
                 {
@@ -57,6 +51,7 @@ export default class MainMapView extends Component {
         } catch (e) {
             console.log(e);
         }
+
     }
 
     getPosition(){
@@ -91,6 +86,74 @@ export default class MainMapView extends Component {
         }
     }
 
+    async DistancePrioritize(currentLat,currentLong){
+        try {
+            const value = await AsyncStorage.getItem('data');
+            if (value !== null){
+                // We have data!!
+                let val = JSON.parse(value);
+
+                var DistAway = [];
+                for(var i = 0; i<val.results.length; i++){
+                    DistAway.push({
+                        location:val.results[i].name,
+                        distanceAway: this.DistanceCalc(currentLat,currentLong,val.results[i].lat,val.results[i].long)
+                    });
+                }
+                DistAway.sort(function(a,b){
+                    if(a.distanceAway<b.distanceAway){
+                        return -1;
+                    }
+                    if(a.distanceAway>b.distanceAway){
+                        return 1;
+                    }
+                    return 0;
+                });
+                return DistAway.slice(0,10);
+            }
+        } catch (error) {
+            // Error retrieving data
+            console.log(error.message)
+        }
+    }
+
+    //calculates distance between two lat long points and returns the distance apart in feet
+    DistanceCalc([lat1, lon1], [lat2, lon2]){
+        // haversine :: (Num, Num) -> (Num, Num) -> Num
+        let haversine = ([lat1, lon1], [lat2, lon2]) => {
+            // Math lib function names
+            let [pi, asin, sin, cos, sqrt, pow, round] =
+                    ['PI', 'asin', 'sin', 'cos', 'sqrt', 'pow', 'round']
+                        .map(k => Math[k]),
+
+            // degrees as radians
+                [rlat1, rlat2, rlon1, rlon2] = [lat1, lat2, lon1, lon2]
+                    .map(x => x / 180 * pi),
+
+                dLat = rlat2 - rlat1,
+                dLon = rlon2 - rlon1,
+                radius = 6372.8; // km
+
+            // km
+            return round(
+                    radius * 2 * asin(
+                        sqrt(
+                            pow(sin(dLat / 2), 2) +
+                            pow(sin(dLon / 2), 2) *
+                            cos(rlat1) * cos(rlat2)
+                        )
+                    ) * 100
+                ) / 100;
+        };
+
+        // Return in feet (converts km to ft)
+        function FeetConverter(km){return km * 3280.84;}
+        return FeetConverter(haversine(
+            [lat1,lon1],
+            [lat2,lon2]
+        ));
+    }
+
     static get defaultProps() {
         return {
             title: 'MapView',
@@ -98,13 +161,10 @@ export default class MainMapView extends Component {
     }
 
     render() {
-        if(!this.state.loaded){
-            return (
-                <LoadingView/>
-            );
-        }
-        else if(this.state.loaded && this.state.initialPosition != 'unknown'){
-            //insert DistancePrioritize(lat,long) function here
+        if(this.state.loaded && this.state.initialPosition != 'unknown'){
+            let val2 = DistancePrioritize(this.state.initialPosition.coords.latitude, this.state.initialPosition.coords.longitude);
+            console.log(val2);
+
             return (
                 <View style={styles.container}>
                     <MapView style={styles.map}
@@ -136,32 +196,7 @@ export default class MainMapView extends Component {
         }
         else {
             return (
-                <View style={styles.container}>
-                    <MapView style={styles.map}
-                             initialRegion={{
-                                latitude: 34.070286,
-                                longitude: -118.443413,
-                                latitudeDelta: 0.0122,
-                                longitudeDelta: 0.0921,}}>
-                        <MapView.Marker
-                            image={require('../../assets/images/pin_1x.png')}
-                            coordinate={{
-                                latitude: 34.070286,
-                                longitude: -118.443413,
-                                latitudeDelta: 0.0122,
-                                longitudeDelta: 0.0921,}}
-                        />
-                    </MapView>
-                    <View style={styles.info}>
-
-                        <ListView
-                            style={{width: width - 40}}
-                            dataSource={this.state.dataSource}
-                            renderRow={(rowData) => <Text>{rowData}</Text>}
-                            enableEmptySections={true}
-                        />
-                    </View>
-                </View>
+                <LoadingView/>
             );
         }
     }
