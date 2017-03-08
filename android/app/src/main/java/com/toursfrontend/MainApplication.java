a5a4ecb0bbfec760087d13e2479adbfc05a2eb84
Yolanda <yolandaxiao7@gmail.com>
2017-03-08 15:58:23 -0800

    update

--- android/app/src/main/java/com/toursfrontend/MainApplication.java
+++ android/app/src/main/java/com/toursfrontend/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNSpinkitPackage(),
           new MapsPackage()
       );
     }
