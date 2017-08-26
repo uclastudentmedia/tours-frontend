3c7cb9afb8f3903a2ac98484f2e67a0c2421c38b
m1c0l <michael.li11702@gmail.com>
2017-08-25 23:27:09 -0700

    Getters for category, tours apis

--- app/Data.js
+++ app/Data.js
@@ -1,11 +1,13 @@
 import 'isomorphic-fetch';
 
-const API_DOMAIN = 'https://tours.bruinmobile.com'
+const API_DOMAIN = 'https://tours.bruinmobile.com';
 
 // API endpoints
 const ENDPOINTS = {
   LANDMARKS: '/api/landmark/',
-}
+  CATEGORIES: '/api/category/',
+  TOURS: '/api/tour/'
+};
 
 
 /**
@@ -69,4 +71,36 @@ export async function GetLandmark(id) {
    */
   return getData(ENDPOINTS.LANDMARKS)
     .then(landmarks => find(landmarks, 'id', id));
+}
+
+export async function GetCategoryList() {
+  /**
+   * @return Array of Category objects
+   */
+  return getData(ENDPOINTS.CATEGORIES);
+}
+
+export async function GetCategory(id) {
+  /**
+   * @param id int
+   * @return Category object
+   */
+  return getData(ENDPOINTS.CATEGORIES)
+    .then(categories => find(categories, 'id', id));
+}
+
+export async function GetTourList() {
+  /**
+   * @return Array of Tour objects
+   */
+  return getData(ENDPOINTS.TOURS);
+}
+
+export async function GetTour(id) {
+  /**
+   * @param id int
+   * @return Tour object
+   */
+  return getData(ENDPOINTS.TOURS)
+    .then(tours => find(tours, 'id', id));
 }
