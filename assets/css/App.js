aaaacd0345be13d0e23200d2592e48942579b322
Richard Sun <richardsun29@ucla.edu>
2017-09-06 00:08:54 -0700

    use TabNavigator

---
+++ assets/css/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  tab: {
+
+  },
+  indicator: {
+
+  },
+  label: {
+
+  },
+  icon: {
+
+  },
+  tabbar: {
+    backgroundColor: '#246dd5',
+  }
+});
